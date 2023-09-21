1d01dc17a93b8c63af470c2d69970a54f23e642c
mianhusnain1 <mianhusnain550@gmail.com>
2023-09-21 20:47:18 +0500

    Validator

--- lib/screens/signup.dart
+++ lib/screens/signup.dart
@@ -5,8 +5,10 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+final _formkey = GlobalKey<FormState>();
+
 class Signup extends StatefulWidget {
-  const Signup({super.key});
+  Signup({super.key});
 
   @override
   State<Signup> createState() => _SignupState();
@@ -41,194 +43,211 @@ class _SignupState extends State<Signup> {
           width: MediaQuery.of(context).size.width,
           child: ListView(
             children: [
-              Column(
-                children: [
-                  const MainTop(),
-                  const Logo(),
-                  const SizedBox(
-                    child: Text(
-                      "SIGN UP",
-                      style: TextStyle(
-                          fontSize: 31.5, fontWeight: FontWeight.bold),
+              Form(
+                key: _formkey,
+                child: Column(
+                  children: [
+                    const MainTop(),
+                    const Logo(),
+                    const SizedBox(
+                      child: Text(
+                        "SIGN UP",
+                        style: TextStyle(
+                            fontSize: 31.5, fontWeight: FontWeight.bold),
+                      ),
                     ),
-                  ),
-                  const SizedBox(
-                    // this is for username
-                    height: 20,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 40, right: 40),
-                    child: Container(
-                      height: 55,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(30),
-                          color: Colors.white,
-                          boxShadow: [
-                            BoxShadow(
-                                color: Colors.grey.shade400,
-                                blurRadius: 4,
-                                spreadRadius: .1,
-                                offset: const Offset(0, 1))
-                          ]),
-                      child: Center(
-                        child: TextField(
-                          cursorColor: myColor,
-                          controller: nameController,
-                          decoration: InputDecoration(
-                              // contentPadding: EdgeInsets.only(top: 13),
-                              prefixIcon: const Icon(
-                                CupertinoIcons.profile_circled,
-                                color: Colors.grey,
-                              ),
-                              hintText: "Full Name",
-                              hintStyle: const TextStyle(color: Colors.grey),
-                              border: InputBorder.none,
-                              focusedBorder: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(30),
-                                  borderSide: BorderSide(color: myColor))),
+                    const SizedBox(
+                      // this is for username
+                      height: 20,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 40, right: 40),
+                      child: Container(
+                        height: 55,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(30),
+                            color: Colors.white,
+                            boxShadow: [
+                              BoxShadow(
+                                  color: Colors.grey.shade400,
+                                  blurRadius: 4,
+                                  spreadRadius: .1,
+                                  offset: const Offset(0, 1))
+                            ]),
+                        child: Center(
+                          child: TextFormField(
+                            cursorColor: myColor,
+                            controller: nameController,
+                            decoration: InputDecoration(
+                                // contentPadding: EdgeInsets.only(top: 13),
+                                prefixIcon: const Icon(
+                                  CupertinoIcons.profile_circled,
+                                  color: Colors.grey,
+                                ),
+                                hintText: "Full Name",
+                                hintStyle: const TextStyle(color: Colors.grey),
+                                border: InputBorder.none,
+                                focusedBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(30),
+                                    borderSide: BorderSide(color: myColor))),
+                            validator: (name) {
+                              if (name!.isEmpty) {
+                                return 'Name is required.';
+                              } else if (name.length < 8) {
+                                return 'Name should have at least 8 characters.';
+                              }
+                              return null;
+                            },
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 40, right: 40),
-                    child: Container(
-                      height: 55,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(30),
-                          color: Colors.white,
-                          boxShadow: [
-                            BoxShadow(
-                                color: Colors.grey.shade400,
-                                blurRadius: 4,
-                                // spreadRadius: .1,
-                                offset: const Offset(1, 1))
-                          ]),
-                      child: Center(
-                        child: TextField(
-                          controller: emailController,
-                          cursorColor: myColor,
-                          decoration: InputDecoration(
-                              prefixIcon: const Icon(
-                                Icons.email,
-                                color: Colors.grey,
-                              ),
-                              hintText: "Email",
-                              hintStyle: const TextStyle(color: Colors.grey),
-                              border: InputBorder.none,
-                              focusedBorder: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(30),
-                                  borderSide: BorderSide(color: myColor))),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 40, right: 40),
+                      child: Container(
+                        height: 55,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(30),
+                            color: Colors.white,
+                            boxShadow: [
+                              BoxShadow(
+                                  color: Colors.grey.shade400,
+                                  blurRadius: 4,
+                                  // spreadRadius: .1,
+                                  offset: const Offset(1, 1))
+                            ]),
+                        child: Center(
+                          child: TextField(
+                            controller: emailController,
+                            cursorColor: myColor,
+                            decoration: InputDecoration(
+                                prefixIcon: const Icon(
+                                  Icons.email,
+                                  color: Colors.grey,
+                                ),
+                                hintText: "Email",
+                                hintStyle: const TextStyle(color: Colors.grey),
+                                border: InputBorder.none,
+                                focusedBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(30),
+                                    borderSide: BorderSide(color: myColor))),
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  const SizedBox(
-                    // this is for username
-                    height: 20,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 40, right: 40),
-                    child: Container(
-                      height: 55,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(30),
-                          color: Colors.white,
-                          boxShadow: [
-                            BoxShadow(
-                                color: Colors.grey.shade400,
-                                blurRadius: 4,
-                                // spreadRadius: .1,
-                                offset: const Offset(1, 1))
-                          ]),
-                      child: Center(
-                        child: TextField(
-                          obscureText: true,
-                          controller: passwordController,
-                          cursorColor: myColor,
-                          decoration: InputDecoration(
-                              prefixIcon: const Icon(
-                                Icons.lock,
-                                color: Colors.grey,
-                              ),
-                              hintText: "Password",
-                              hintStyle: const TextStyle(color: Colors.grey),
-                              border: InputBorder.none,
-                              focusedBorder: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(30),
-                                  borderSide: BorderSide(color: myColor))),
+                    const SizedBox(
+                      // this is for username
+                      height: 20,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 40, right: 40),
+                      child: Container(
+                        height: 55,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(30),
+                            color: Colors.white,
+                            boxShadow: [
+                              BoxShadow(
+                                  color: Colors.grey.shade400,
+                                  blurRadius: 4,
+                                  // spreadRadius: .1,
+                                  offset: const Offset(1, 1))
+                            ]),
+                        child: Center(
+                          child: TextField(
+                            obscureText: true,
+                            controller: passwordController,
+                            cursorColor: myColor,
+                            decoration: InputDecoration(
+                                prefixIcon: const Icon(
+                                  Icons.lock,
+                                  color: Colors.grey,
+                                ),
+                                hintText: "Password",
+                                hintStyle: const TextStyle(color: Colors.grey),
+                                border: InputBorder.none,
+                                focusedBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(30),
+                                    borderSide: BorderSide(color: myColor))),
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  const SizedBox(
-                    // this is for username
-                    height: 20,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 40, right: 40),
-                    child: Container(
-                      height: 55,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(30),
-                          color: Colors.white,
-                          boxShadow: [
-                            BoxShadow(
-                                color: Colors.grey.shade400,
-                                blurRadius: 4,
-                                // spreadRadius: .1,
-                                offset: const Offset(1, 1))
-                          ]),
-                      child: Center(
-                        child: TextField(
-                          obscureText: true,
-                          cursorColor: myColor,
-                          decoration: InputDecoration(
-                              prefixIcon: const Icon(
-                                Icons.lock,
-                                color: Colors.grey,
-                              ),
-                              hintText: "Confirm Password",
-                              hintStyle: const TextStyle(color: Colors.grey),
-                              border: InputBorder.none,
-                              focusedBorder: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(30),
-                                  borderSide: BorderSide(color: myColor))),
+                    const SizedBox(
+                      // this is for username
+                      height: 20,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 40, right: 40),
+                      child: Container(
+                        height: 55,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(30),
+                            color: Colors.white,
+                            boxShadow: [
+                              BoxShadow(
+                                  color: Colors.grey.shade400,
+                                  blurRadius: 4,
+                                  // spreadRadius: .1,
+                                  offset: const Offset(1, 1))
+                            ]),
+                        child: Center(
+                          child: TextField(
+                            obscureText: true,
+                            cursorColor: myColor,
+                            decoration: InputDecoration(
+                                prefixIcon: const Icon(
+                                  Icons.lock,
+                                  color: Colors.grey,
+                                ),
+                                hintText: "Confirm Password",
+                                hintStyle: const TextStyle(color: Colors.grey),
+                                border: InputBorder.none,
+                                focusedBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(30),
+                                    borderSide: BorderSide(color: myColor))),
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  const SizedBox(
-                    height: 40,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 40.0, right: 40),
-                    child: SizedBox(
-                        child: Btn(
-                      title: "SIGN UP",
-                      action: () async {
-                        try {
-                          final auth = FirebaseAuth.instance;
-                          await auth
-                              .createUserWithEmailAndPassword(
+                    const SizedBox(
+                      height: 40,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 40.0, right: 40),
+                      child: SizedBox(
+                          child: Btn(
+                        title: "SIGN UP",
+                        action: () async {
+                          if (_formkey.currentState!.validate()) {
+                            // Form is valid, perform signup
+                            try {
+                              final auth = FirebaseAuth.instance;
+                              await auth
+                                  .createUserWithEmailAndPassword(
                                 email: emailController.text.toString(),
                                 password: passwordController.text.toString(),
                               )
-                              .then((value) => Navigator.push(
+                                  .then((value) {
+                                Navigator.push(
                                   context,
                                   MaterialPageRoute(
                                     builder: (context) => const Verify(),
-                                  )));
-                        } catch (e) {
-                          print("Error is : $e");
-                        }
-                      },
-                    )),
-                  ),
-                ],
+                                  ),
+                                );
+                              });
+                            } catch (e) {
+                              print("Error is : $e");
+                            }
+                          }
+                        },
+                      )),
+                    ),
+                  ],
+                ),
               )
             ],
           )),
