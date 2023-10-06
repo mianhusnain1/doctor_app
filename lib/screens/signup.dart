2a113e1201705b827575dab103d932a871fc7892
Mian Husnain <mianhusnain550@gmail.com>
2023-10-06 23:41:27 +0500

    Updation

--- lib/screens/signup.dart
+++ lib/screens/signup.dart
@@ -54,7 +54,7 @@ class _SignupState extends State<Signup> {
                     const Logo(),
                     const SizedBox(
                       child: Text(
-                        "SIGN UP1",
+                        "SIGN UP",
                         style: TextStyle(
                             fontSize: 31.5, fontWeight: FontWeight.bold),
                       ),
